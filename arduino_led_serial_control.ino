52ef301d057a99c774cf34c224c422a5bd67d609
EVIE 🍒 <126894089+urevie@users.noreply.github.com>
2024-06-23 11:44:56 -0700

    Adding some features (Toggling the LED state, Reading the current LED state and Adding command-line arguments to control the LED)

--- arduino_led_serial_control.ino
+++ arduino_led_serial_control.ino
@@ -1,5 +1,7 @@
+char ledState = '0'; 
 void setup() {
   pinMode(13, OUTPUT); // LED on pin 13
+  digitalWrite(13, LOW); // Ensure LED is off
   Serial.begin(9600);  // Start serial communication at 9600 baud
 }
 
@@ -8,8 +10,20 @@ void loop() {
     char command = Serial.read(); // Read the incoming command
     if (command == '1') {
       digitalWrite(13, HIGH); // Turn the LED on
+      ledState = '1';
     } else if (command == '0') {
       digitalWrite(13, LOW);  // Turn the LED off
+      ledState = '0';
+    } else if (command == 't') {
+      if (ledState == '0') {
+        digitalWrite(13, HIGH);
+        ledState = '1';
+      } else {
+        digitalWrite(13, LOW);
+        ledState = '0';
+      }
+    } else if (command == 'r') {
+      Serial.write(ledState); // Send the current LED state back
     }
   }
 }
--- serial_device_control.c
+++ serial_device_control.c
@@ -16,19 +16,17 @@ int set_interface_attribs(int fd, int speed) {
     cfsetospeed(&tty, speed);
     cfsetispeed(&tty, speed);
 
-    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     
-    tty.c_iflag &= ~IGNBRK;         
-    tty.c_lflag = 0;                
-                                    
-    tty.c_oflag = 0;                
-    tty.c_cc[VMIN]  = 0;            
-    tty.c_cc[VTIME] = 5;            
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; 
+    tty.c_iflag &= ~IGNBRK; 
+    tty.c_lflag = 0; 
+    tty.c_oflag = 0; 
+    tty.c_cc[VMIN]  = 0; 
+    tty.c_cc[VTIME] = 5; 
 
     tty.c_iflag &= ~(IXON | IXOFF | IXANY); 
 
-    tty.c_cflag |= (CLOCAL | CREAD);    
-                                        
-    tty.c_cflag &= ~(PARENB | PARODD);  
+    tty.c_cflag |= (CLOCAL | CREAD); 
+    tty.c_cflag &= ~(PARENB | PARODD); 
     tty.c_cflag &= ~CSTOPB;
     tty.c_cflag &= ~CRTSCTS;
 
@@ -48,14 +46,28 @@ void set_blocking(int fd, int should_block) {
     }
 
     tty.c_cc[VMIN]  = should_block ? 1 : 0;
-    tty.c_cc[VTIME] = 5;            
+    tty.c_cc[VTIME] = 5; 
 
     if (tcsetattr(fd, TCSANOW, &tty) != 0)
         perror("tcsetattr");
 }
 
-int main() {
-    const char *portname = "/dev/ttyACM0"; // Change me !!
+void print_usage(const char *progname) {
+    printf("Usage: %s <command>\n", progname);
+    printf("Commands:\n");
+    printf("  on     - Turn the LED on\n");
+    printf("  off    - Turn the LED off\n");
+    printf("  toggle - Toggle the LED state\n");
+    printf("  read   - Read the LED state\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    const char *portname = "/dev/ttyACM0"; // Change this to your port
     int fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
     if (fd < 0) {
         perror("open");
@@ -65,11 +77,32 @@ int main() {
     set_interface_attribs(fd, B9600); 
     set_blocking(fd, 0);              
 
-    write(fd, "1", 1);
-    sleep(2); 
-
-    
-    write(fd, "0", 1);
+    const char *command = argv[1];
+    if (strcmp(command, "on") == 0) {
+        write(fd, "1", 1);
+    } else if (strcmp(command, "off") == 0) {
+        write(fd, "0", 1);
+    } else if (strcmp(command, "toggle") == 0) {
+        write(fd, "t", 1);
+    } else if (strcmp(command, "read") == 0) {
+        write(fd, "r", 1);
+        sleep(1); 
+        char buf[1];
+        int n = read(fd, buf, sizeof buf);
+        if (n > 0) {
+            if (buf[0] == '1') {
+                printf("LED is on\n");
+            } else if (buf[0] == '0') {
+                printf("LED is off\n");
+            } else {
+                printf("Unknown state: %c\n", buf[0]);
+            }
+        } else {
+            printf("Failed to read LED state\n");
+        }
+    } else {
+        print_usage(argv[0]);
+    }
 
     close(fd);
     return 0;
